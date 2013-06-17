851cc64291c55c4bfa0cb2912527fec5cac9bc02
Erhan Abay <erhanabay@gmail.com>
2013-06-17 10:54:35 +0300

    vm provisioning with vagrant+puppet
    installs:
    * ant
    * android sdk with latest platform
    * nodejs
    * npm cordova package and its dependencies

---
+++ .puppet/manifests/main.pp
@@ -0,0 +1,40 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+include java
+
+class { 'ant':
+  version => '1.9.1'
+}
+
+class { 'android':
+  version => '22.0.1',
+  user    => 'vagrant',
+  group   => 'vagrant'
+}
+
+android::platform { 'android-17': }
+
+exec { 'android path':
+  command => "echo 'export PATH=${android::params::installdir}/android-sdk-linux/tools:${android::params::installdir}/android-sdk-linux/platform-tools:\$PATH' >> /home/vagrant/.bash_aliases",
+  user    => 'vagrant',
+  unless  => "which android >/dev/null 2>&1",
+  require => Class['android']
+}
+
+include apt
+
+apt::ppa { 'ppa:chris-lea/node.js': }
+->
+package { 'nodejs':
+  ensure => latest
+}
+
+package { 'cordova':
+  ensure => installed,
+  provider => npm
+}
+
+#cordova package dependencies
+package { ['curl', 'unzip']:
+  ensure => latest
+}
